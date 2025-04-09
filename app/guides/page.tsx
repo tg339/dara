20a8b6e4fd6e206edf5c2bfa3eeaccba11a59a66
Tim Delisle <tim@fiveonefour.com>
2025-04-08 21:50:50 -0700

    build fix

---
+++ app/guides/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function GuidesPage() {
+  return (
+    <div>
+      <h1>Guides Page Placeholder</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
