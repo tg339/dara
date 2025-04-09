a8776983e25dd241d918d5c800cfc40726d3b198
Tim Delisle <tim@fiveonefour.com>
2025-04-08 21:53:26 -0700

    buid fix 2=

---
+++ app/practitioner-insights/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PractitionerInsightsPage() {
+  return (
+    <div>
+      <h1>Practitioner Insights Page Placeholder</h1>
+      <p>This page is under construction.</p>
+    </div>
+  );
+}
